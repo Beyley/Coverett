7a01dd6ba84385a4768d7e978e09e9e8736fef34
jvyden <jvyden@jvyden.xyz>
2023-04-16 23:03:12 -0400

    Formatting

--- lsitems.c
+++ lsitems.c
@@ -7,24 +7,24 @@
 
 int main(int argc, char* argv[])
 {
-	if (argc < 2){
-		puts("LiSt Items");
-		puts("Usage: lsitems <side>");
-		return 0;
-	}
+    if (argc < 2){
+        puts("LiSt Items");
+        puts("Usage: lsitems <side>");
+        return 0;
+    }
 
     bus_t stdbus = openBus("/dev/hvc0");
 
     if(stdbus == NULL) {
-		fputs("Failed to open bus.\n", stderr);
-		return -1;
-	}
+        fputs("Failed to open bus.\n", stderr);
+        return -1;
+    }
 
     device_t dev = findDev(stdbus, "inventory_operations");
-	if (!dev.exists){
-		fputs("This program requires a Inventory Operations Module.\n", stderr);
-		return -1;
-	}
+    if (!dev.exists){
+        fputs("This program requires a Inventory Operations Module.\n", stderr);
+        return -1;
+    }
 
     result_t res = getItems(&dev, argv[1]);
 
@@ -33,7 +33,7 @@ int main(int argc, char* argv[])
         if(cJSON_IsNull(item)) continue;
         
         char* id = strdup(cJSON_GetObjectItemCaseSensitive(item, "id")->valuestring);
-	    int count = cJSON_GetObjectItemCaseSensitive(item, "Count")->valueint;
+        int count = cJSON_GetObjectItemCaseSensitive(item, "Count")->valueint;
 
         printf("(%ix %s)\n", count, id);
     }
